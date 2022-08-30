792fae0a1de2ec03011edb5cd44b7356fe630398
PhilippAusHeidelberg <110686148+PhilippAusHeidelberg@users.noreply.github.com>
2022-08-30 18:54:50 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,13 @@
+/**
+ * das ist tim
+ */
+input.onButtonPressed(Button.A, function () {
+    basic.showString("ha ha ha ha ha ! ")
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        # # # # #
+        . . . # #
+        . . . # #
+        `)
+})
